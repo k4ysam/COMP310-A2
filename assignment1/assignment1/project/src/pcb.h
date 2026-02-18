e5a62319abd5c689f36897f8d0ebccba20f37154
k4ysam <ksamaksh0601@gmail.com>
2026-02-18 03:36:10 +0000

    add pcb.h

--- assignment1/assignment1/project/src/pcb.h
+++ assignment1/assignment1/project/src/pcb.h
@@ -2,17 +2,17 @@
 #define PCB_H
 
 struct PCB {
-    int pid;              /* unique process id */
-    int mem_start;        /* first frame index in the frame store */
-    int mem_len;          /* number of frames (lines) belonging to this process */
-    int pc;               /* program counter: offset from mem_start, 0-based */
-    int job_length_score; /* used by SJF / AGING policies */
-    struct PCB *next;     /* intrusive link for the ready queue */
+    int pid;              //unique pid
+    int mem_start;        //first frame index in the frame store
+    int mem_len;          //number of frames (lines) belonging to this process
+    int pc;               //program counter: starts at 0 and increments for each instruction
+    int job_length_score; //initialised to mem_len, used by SJF / AGING
+    struct PCB *next;     //intrusive pointer so PCB can link itself to the ready queue without extra allocations
 };
 
 typedef struct PCB PCB;
 
-PCB *pcb_create(int mem_start, int mem_len);
+PCB *pcb_create(int mem_start, int mem_len);  // mallocs a PCB, fills in the fields and returns a pointer to the PCB
 void pcb_free(PCB *pcb);
 
 #endif
